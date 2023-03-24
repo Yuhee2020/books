3ba64f9397feecfd413db314b8287b76d6699212
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2023-03-24 10:00:00 +0300

    feat:added env.example, booksReducer.test

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: "https://www.googleapis.com/books/v1/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   params:{
-    key: 'AIzaSyAdONq-W5wH0GEfSPe0E8bEnHEwOovO9SY',
+    key: process.env.REACT_APP_GOOGLE_BOOKS_API_KEY,
   }
 })
 
